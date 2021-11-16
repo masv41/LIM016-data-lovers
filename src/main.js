940be461572a96798e56a1e6659302c5d2d8bd86
LindsayMuji <lmelissa.mp@gmail.com>
2021-11-16 17:56:02 -0500

    modificacion HTML color Body y imagen titulo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(data.results[0].name);
+
+/*let foto = document.getElementById("imagen2");
+  foto.src = data.results[0].image;
