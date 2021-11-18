45424f4b87e348170026802bd58217ef39332043
LindsayMuji <lmelissa.mp@gmail.com>
2021-11-17 22:32:54 -0500

    ingreso de flexbox para personajes

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(data.results[0].name);
 
-/*let foto = document.getElementById("imagen2");
+let foto = document.getElementById("personaje1");
   foto.src = data.results[0].image;
