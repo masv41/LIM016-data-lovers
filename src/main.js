5dbfc0002dfef19eeda312eb831d8ba37d203f96
LindsayMuji <lmelissa.mp@gmail.com>
2021-11-19 21:27:23 -0500

    funcionalidad JS ingreso de imagenes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,34 @@
-import { example } from './data.js';
-import data from './data/rickandmorty/rickandmorty.js';
+import data from "./data/rickandmorty/rickandmorty.js";
 
-console.log(data.results[0].name);
+console.log(data.results);
+let arrayData = [];
+arrayData = data.results;
+let personaje, foto, nombre, textoNombre;
+let pprincipales = document.getElementById("personajes");
+navegarData ();
 
-let foto = document.getElementById("personaje1");
-  foto.src = data.results[0].image;
+function navegarData (){
+  arrayData.map(element => imprimirPersonajes(element));
+}
+
+function imprimirPersonajes (arrayData){
+  console.log("arreglo", arrayData);
+  console.log("id", pprincipales);
+  personaje = document.createElement("div");
+  pprincipales.appendChild(personaje);
+  foto = document.createElement("img");
+  personaje.appendChild(foto);
+  foto.src = arrayData.image;
+  nombre = document.createElement("p");
+  personaje.appendChild(nombre);
+  textoNombre = document.createTextNode(arrayData.name);
+  nombre.appendChild(textoNombre);
+
+}
+
+
+//console.log (data.results[i].image);
+
+//let foto = document.getElementById("personaje1");
+
+//foto.src = data.results[0].image;
