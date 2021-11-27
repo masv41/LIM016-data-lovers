b478444fee7f54610c3712d44c9ad56729f54ae0
LindsayMuji <lmelissa.mp@gmail.com>
2021-11-26 22:09:30 -0500

    print filter

--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ import data from "./data/rickandmorty/rickandmorty.js";
 let arrayData = [];
 arrayData = data.results;
 let mainCharacters = document.getElementById("characters");
-navegarData();
+printData(arrayData);
 
 
 // tample string para poder dar estilo en CSS
-function navegarData() {
+function printData(data) {
  let profiles=''
-  arrayData.forEach(function(element){
+  data.forEach(function(element){
     profiles+=
     `<div class= "cont-personajes">
       <div class= "card">
@@ -22,35 +22,33 @@ function navegarData() {
         <ul>
           <p><b>Nombre:<b> ${element.name}</p>
           <p><b>Estado:<b> ${element.status}</p>
-          <p><b>Especie:<b> ${element.specie}</p>
+          <p><b>Especie:<b> ${element.species}</p>
           <p><b>Genero:<b> ${element.gender}</p>
           <p><b>Origen:<b> ${element.origin.name}</p>
         </ul>
       </div>
     </div>`
   })
-  //imprimir en mainCharacters(personajes de HTML)la data de perfiles
+  //imprimir en mainCharacters(personajes de HTML)la data de profiles
   mainCharacters.innerHTML = profiles;
 }
 
 
 //let getResults = document.getElementById("applyFilters")
 
-
 let genderChoice = document.getElementById("gender")
+
 //evento que cambia segun la seleccion en la barra de navegación. (e.target.value) obtener el valor de los cambios del select
 genderChoice.addEventListener('change',(e) =>{
-  console.log(e.target.value);
+
 //filterDataByGender from data.js --- (arrayData, (e.target.value)) son argumentos
 //"let filterGender = arrayData.filter(person => person.species === (e.target.value));" funcion llevada a Data.js
 let filterGender = filterDataByGender(arrayData, (e.target.value));
- // console.log(filterGender);
-let printFilter = navegarData(filterGender);
-document.getElementById("characters").innerHTML= printFilter
-  console.log(printFilter)
+
+  printData(filterGender);
 })
 
-let specieChoice = document.getElementById("species")
+/*let specieChoice = document.getElementById("species")
 specieChoice.addEventListener('change',(e) =>{
   console.log(e.target.value);
 let filterSpecie = filterDataBySpecie(arrayData, (e.target.value));
@@ -63,20 +61,6 @@ statusChoice.addEventListener('change',(e) =>{
 let filterStatus = filterDataByStatus(arrayData, (e.target.value));
   //console.log(filterStatus);
 
-})
-
-/*let printFilter = navegarData(genderChoice);
-document.getElementById("profiles").innerHTML= printFilter
-   console.log(printFilter)*/
-
-
-
- /* let speciesChoice = document.getElementById("species").value
-  let statusChoice = document.getElementById("status").value
-  let filterResults = func.filterData(person, genderChoice, speciesChoice, statusChoice)
-  let orderChoice = document.getElementById("alphabet").value
-  let orderedResults = func.sortData(filterResults, orderChoice)
-  let renderedData = navegarData(orderedResults)
-  document.getElementById("perfiles").innerHTML=renderedData*/
+})*/
 
 
