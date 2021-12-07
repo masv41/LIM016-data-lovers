fbb2cb178a1370c030e110cb93df8fc227e474a7
masv41 <mariangelsanchez41@gmail.com>
2021-12-07 07:41:43 -0500

    Responsive primera versión

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { filterDataByGender, sortAZData, sortZAData } from "./data.js";
+import { filterDataByGender, sortAZData, sortZAData, multiplerFilter } from "./data.js";
 
 
 
@@ -16,3 +16,37 @@ function filtradoPorOrden(seleccion, localData) {
     }
     return (filtrarOrden)
    }
+
+   /*Aplicar todos los filtros*/
+
+   function(data){
+    let person = data.results
+    let alive=func.filterData(person, "none", "none", "Alive")
+    let dead=func.filterData(person, "none", "none", "Dead")
+    let unknown=func.filterData(person, "none", "none", "unknown")
+    let porcentageA=multiplerFilter.computeData(person, alive)
+    let porcentageD=multiplerFilter.computeData(person, dead)
+    let porcentageU=multiplerFilter.computeData(person, unknown)
+
+   let personFormat=renderData(person)
+   document.getElementById("profiles").innerHTML=personFormat
+   document.getElementById("numberOfResults").innerHTML=person.length + " resultados"
+
+   let getResults=document.getElementById("applyFilters")
+   getResults.addEventListener('click',() =>{
+       let genderChoice=document.getElementById("gender").value
+       let speciesChoice=document.getElementById("species").value
+       let statusChoice=document.getElementById("status").value
+       let filterResults=func.filterData(person, genderChoice, speciesChoice, statusChoice)
+       let numberResults=filterResults.length
+       document.getElementById("numberOfResults").innerHTML=numberResults + " resultados"
+       let orderChoice=document.getElementById("alphabet").value
+       let orderedResults=func.sortData(filterResults, orderChoice)
+       let renderedData=renderData(orderedResults)
+       document.getElementById("profiles").innerHTML=renderedData
+   })
+
+let clean = document.getElementById("clean")
+clean.addEventListener('click',() => {
+   location.reload()
+})
